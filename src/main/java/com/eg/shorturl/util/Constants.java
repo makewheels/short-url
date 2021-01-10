4b223d0baec8806f9d7ecbd4683990e4d9b30cb9
makewheels <makewheels>
2021-01-11 00:16:52 +0800

    SHORT_ID_LENGTH = 2

--- src/main/java/com/eg/shorturl/util/Constants.java
+++ src/main/java/com/eg/shorturl/util/Constants.java
@@ -5,6 +5,6 @@ package com.eg.shorturl.util;
  * @Time 2021.01.10 23:21:31
  */
 public class Constants {
-    public static final int SHORT_ID_LENGTH = 1;
+    public static final int SHORT_ID_LENGTH = 2;
     public static final String BASE_URL = "http://java8.icu/";
 }

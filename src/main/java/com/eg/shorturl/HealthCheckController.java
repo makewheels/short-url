6b2eac061ccec0867f291413a52ceb8d1a93faa4
makewheels <makewheels@github.com>
2022-04-06 23:46:01 +0800

    itube.work

---
+++ src/main/java/com/eg/shorturl/HealthCheckController.java
@@ -0,0 +1,12 @@
+wpackage com.eg.shorturl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("healthCheck")
+    public String healthCheck() {
+        return "ok " + System.currentTimeMillis();
+    }
+}
